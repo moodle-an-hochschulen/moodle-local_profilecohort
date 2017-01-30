e605c4845a6d6642d4f37caa3f9a9185fb795dd0
Alexander Bias <alexander.bias@uni-ulm.de>
2017-01-30 08:03:47 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_profilecohort';
-$plugin->version = 2016091601;
-$plugin->release = 'v3.0-r1';
-$plugin->requires = 2015111600;
+$plugin->version = 2017013000;
+$plugin->release = 'v3.2-r1';
+$plugin->requires = 2016120500;
 $plugin->maturity = MATURITY_STABLE;
