7749f8bdb3d30989099509ee86f422cbce9850b8
Davo Smith <davo.smith@synergy-learning.com>
2019-11-27 14:40:32 +0000

    #21 do not add deleted users to cohorts
    Also removes incorrectly added users during upgrade

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_profilecohort';
-$plugin->version = 2019070900;
+$plugin->version = 2019112700;
 $plugin->release = 'v3.7-r1';
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_STABLE;
