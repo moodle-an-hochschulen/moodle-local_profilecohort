4bb0c2717d556fb2a3ac84741e7d9d9b5af0c4a7
Alexander Bias <alexander.bias@uni-ulm.de>
2021-02-15 08:46:48 +0100

    Release v3.10-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_profilecohort';
-$plugin->version = 2021010900;
-$plugin->release = 'v3.10-r1';
+$plugin->version = 2021010901;
+$plugin->release = 'v3.10-r2';
 $plugin->requires = 2020110900;
 $plugin->supported = [310, 310];
 $plugin->maturity = MATURITY_STABLE;
