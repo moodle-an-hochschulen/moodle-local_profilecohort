1b99fd10bc4255daea31f82a15ba3927e6bd5a4d
Alexander Bias <bias@alexanderbias.de>
2022-06-29 20:38:28 +0200

    Release v3.11-r2

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_profilecohort';
-$plugin->version = 2021061300;
-$plugin->release = 'v3.11-r1';
+$plugin->version = 2021061301;
+$plugin->release = 'v3.11-r2';
 $plugin->requires = 2021051700;
 $plugin->supported = [311, 311];
 $plugin->maturity = MATURITY_STABLE;
